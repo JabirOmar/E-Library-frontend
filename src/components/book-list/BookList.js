f52c270ad443431c7acdf04206aeae862832d223
Anna-Ashton <ann.karanja@student.moringaschool.com>
2022-11-10 17:46:31 +0300

    Included a variable for api

--- src/components/book-list/BookList.js
+++ src/components/book-list/BookList.js
@@ -4,68 +4,118 @@ import axios from "axios";
 // import { Link } from'react-router-dom'
 
 function BookList() {
-  const [addedBooks, setAddedBooks] = useState([]);
+  // const [books, setBooks] = useState([]);
 
-  function loadBooks() {
-    axios
-      .get("https://rocky-river-97920.herokuapp.com/books")
-      .then((response) => {
-        setAddedBooks(response.data.reverse());
-      });
-  }
-  // render() {
+  const [books, setBooks] = useState([]);
+    const apiEndPoint = 'https://rocky-river-97920.herokuapp.com/books'
+    useEffect (() => {
 
-  useEffect(() => {
-    loadBooks();
-  }, []);
+        const getBooks = async () => {
+            const {data:res }= await axios.get(apiEndPoint)
+            setBooks(res)
+        }
+        getBooks();
+    },[])
 
-  // function deleteBook(id) {
-  //   axios.delete(`https://rocky-river-97920.herokuapp.com/books/${id}`).then(loadBooks());
+  // function loadBooks() {
+  //   axios
+  //     .get("https://rocky-river-97920.herokuapp.com/books")
+  //     .then((response) => {
+  //       setAddedBooks(response.data.reverse());
+  //     });
   // }
-
-  return (
-    <div>
-      <div className="list_title">
-        <h2>Book List</h2>
+  // render() {
+    return (
+      <div>
+          <h1 className='list_title'>We have list of <span>{books.length}</span> Books</h1>
+  
+          <div className='list_container'>
+  
+             {
+              books.map( book => {
+                return(
+                  <div key={book.id} className='list_card'>
+                    <div className='info'>
+                      <span className='main_title'>Book Name<i> : </i></span>
+                      <p>{book.title}</p>
+                    </div>
+  
+                    <div className='info'>
+                      <span className='main_title'>Author<i> : </i></span>
+                      <p>{book.author}</p>
+                    </div>
+  
+                    <div className='info'>
+                      <span className='main_title'>Description<i> : </i></span>
+                      <p>{book.description}</p>
+                    </div>
+  
+                    <div className='info'>
+                      <span className='main_title'>Total pages<i> : </i></span>
+                      <p>{book.pages}</p>
+                    </div>
+                </div> 
+                )
+              })
+            }
+  
+          </div>
+  
+  
       </div>
-      <div className="list_container">
-        {addedBooks.map((data, index) => {
-          return (
-            <div key={index} className="list_card">
-              <div className="info">
-                <span className="main_title">
-                  Book Name<i> : </i>
-                </span>
-                <p>{data.title}</p>
-              </div>
+  
 
-              <div className="info">
-                <span className="main_title">
-                  Author<i></i>
-                </span>
-                <p>{data.author}</p>
-              </div>
-              {/* <div> 
-                    <span className="main_title">Author</span>
-                  <p>{data.author}</p>
-                  </div> */}
-              <div className="info">
-                <span className="main_title">
-                  Description<i></i>
-                </span>
-                <p>{data.description}</p>
-              </div>
-              <div className="info">
-                <span className="main_title">
-                  Total pages<i></i>
-                </span>
-                <p>{data.pages}</p>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </div>
+//   useEffect(() => {
+//     loadBooks();
+//   }, []);
+
+//   // function deleteBook(id) {
+//   //   axios.delete(`https://rocky-river-97920.herokuapp.com/books/${id}`).then(loadBooks());
+//   // }
+
+//   return (
+//     <div>
+//       <div className="list_title">
+//         <h2>Book List</h2>
+//       </div>
+//       <div className="list_container">
+//         {addedBooks.map((data, index) => {
+//           return (
+//             <div key={index} className="list_card">
+//               <div className="info">
+//                 <span className="main_title">
+//                   Book Name<i> : </i>
+//                 </span>
+//                 <p>{data.title}</p>
+//               </div>
+
+//               <div className="info">
+//                 <span className="main_title">
+//                   Author<i></i>
+//                 </span>
+//                 <p>{data.author}</p>
+//               </div>
+//               {/* <div> 
+//                     <span className="main_title">Author</span>
+//                   <p>{data.author}</p>
+//                   </div> */}
+//               <div className="info">
+//                 <span className="main_title">
+//                   Description<i></i>
+//                 </span>
+//                 <p>{data.description}</p>
+//               </div>
+//               <div className="info">
+//                 <span className="main_title">
+//                   Total pages<i></i>
+//                 </span>
+//                 <p>{data.pages}</p>
+//               </div>
+//             </div>
+//           );
+//         })}
+//       </div>
+//     </div>
   );
 }
 
