7899a45b896a8a7cdac93e0cdce021b2c9621ab9
Anna-Ashton <ann.karanja@student.moringaschool.com>
2022-11-10 11:47:22 +0300

    Removed class component,included react hooks, fetched books from api

--- src/components/book-list/BookList.js
+++ src/components/book-list/BookList.js
@@ -1,40 +1,72 @@
-import React, { Component } from 'react'
-import './booklist.css'
+import React, { useState, useEffect } from "react";
+import "./booklist.css";
+import axios from "axios";
+// import { Link } from'react-router-dom'
 
-export class BookList extends Component {
+function BookList() {
+  const [addedBooks, setAddedBooks] = useState([]);
 
-  render() {
-    return (
-      <div>
-        <div className='list_title'>
-          <h2>Book List</h2>
-        </div>
-        <div className='list_container'>
-          <div className='list_card'>
-             <div>
-              <span className='main_title'>Book Name</span>
-              <p>The King</p>
-             </div>
-
-             <div>
-              <span className='main_title'>Author</span>
-              <p>John</p>
-             </div>
-
-             <div>
-              <span className='main_title'>Description</span>
-              <p>Political Book</p>
-             </div>
-             
-             <div>
-              <span className='main_title'>Total pages</span>
-              <p>300</p>
-             </div>
-          </div>
-        </div>
-      </div>
-    )
+  function loadBooks() {
+    axios
+      .get("https://rocky-river-97920.herokuapp.com/books")
+      .then((response) => {
+        setAddedBooks(response.data.reverse());
+      });
   }
+  // render() {
+
+  useEffect(() => {
+    loadBooks();
+  }, []);
+
+  // function deleteBook(id) {
+  //   axios.delete(`https://rocky-river-97920.herokuapp.com/books/${id}`).then(loadBooks());
+  // }
+
+  return (
+    <div>
+      <div className="list_title">
+        <h2>Book List</h2>
+      </div>
+      <div className="list_container">
+        {addedBooks.map((data, index) => {
+          return (
+            <div key={index} className="list_card">
+              <div className="info">
+                <span className="main_title">
+                  Book Name<i> : </i>
+                </span>
+                <p>{data.title}</p>
+              </div>
+
+              <div className="info">
+                <span className="main_title">
+                  Author<i></i>
+                </span>
+                <p>{data.author}</p>
+              </div>
+              {/* <div> 
+                    <span className="main_title">Author</span>
+                  <p>{data.author}</p>
+                  </div> */}
+              <div className="info">
+                <span className="main_title">
+                  Description<i></i>
+                </span>
+                <p>{data.description}</p>
+              </div>
+              <div className="info">
+                <span className="main_title">
+                  Total pages<i></i>
+                </span>
+                <p>{data.pages}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default BookList
+export default BookList;
