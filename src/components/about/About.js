3bab42533c3f2c0f1111bca1e1bf6f64a8156dc7
Anna-Ashton <ann.karanja@student.moringaschool.com>
2022-11-12 11:52:04 +0300

    imported about.css to about.js

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,8 +1,78 @@
 import React from 'react'
+import './about.css'
 
 function About() {
   return (
-    <div>About</div>
+    <>
+    <div className='about_section'>
+    <h3>About Us</h3>
+    <hr id='horizontal_line' />
+    </div>
+
+    <div className='about_info_container'>
+      <div className='about_info'>
+        <div>
+          <p>Name : <span className='names'>Jabir Omar</span></p>
+        </div>
+        <div>
+          <p>Title : Frontend Developer <br/> (Scrum master)</p>
+        </div>
+        <div>
+          <p>Duty : Reactjs User Interface</p>
+        </div>
+      </div>
+
+      <div className='about_info'>
+        <div>
+          <p>Name : <span className='names'>Ann Karanja</span></p>
+        </div>
+        <div>
+          <p>Title : Frontend Developer</p>
+        </div>
+        <div>
+          <p>Duty : API endPionts</p>
+        </div>
+      </div>
+
+      <div className='about_info'>
+        <div>
+          <p>Name : <span className='names'>Corrine Muriuki</span></p>
+        </div>
+        <div>
+          <p>Title : Backend Developer</p>
+        </div>
+        <div>
+          <p>Duty : DB config, Active Records, Migrations</p>
+        </div>
+      </div>
+
+      <div className='about_info'>
+        <div>
+          <p>Name : <span className='names'>Terah Mumuli</span></p>
+        </div>
+        <div>
+          <p>Title : Backend Developer</p>
+        </div>
+        <div>
+          <p>Duty : Routes</p>
+        </div>
+      </div>
+
+      <div className='about_info'>
+        <div>
+          <p>Name : <span className='names'>Esther Nganga</span></p>
+        </div>
+        <div>
+          <p>Title : Backend Developer</p>
+        </div>
+        <div>
+          <p>Duty : Models</p>
+        </div>
+      </div>
+      
+    </div>
+    
+    </>
   )
 }
 
